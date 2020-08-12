b4770b7a2afe291db23ba970748102b3294b8d59
Keith Mendoza <keith@homepluspower.info>
2020-08-11 18:33:39 -0700

    Fix up links

--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -4,41 +4,30 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
 } from 'react-router-dom';
 
 import { Load } from './load';
 import { NoMenu } from './nomenu';
 
+function App() {
+    return (
+        <Switch>
+            <Route exact path="/load">
+                <Load />
+            </Route>
+            <Route exact path="/nomenu">
+                <NoMenu />
+            </Route>
+            <Route>
+                <p>Use the menu</p>
+            </Route>
+        </Switch>
+    );
+}
+
 ReactDOM.render(
-  <Router>
-    <nav className="navbar navbar-expand">
-      <Link className="navbar-brand" to="/">Navbar</Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link" to="/load">Load</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/nomenu">No Menu</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <Switch>
-      <Route exact path="/load">
-        <Load />
-      </Route>
-      <Route exact path="/nomenu">
-        <NoMenu />
-      </Route>
-      <Route>
-          <p>Use the menu</p>
-      </Route>
-    </Switch>
-  </Router>,
-  document.getElementById('root')
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
 );

10f4729ff2636efc9ae21041805ce6a33119ba78
Keith Mendoza <keith@homepluspower.info>
2021-01-19 09:51:08 -0800

    Add libhttpserver as an external project

--- app/backend/src/main.cpp
+++ app/backend/src/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <memory>
 
 #include <log4cplus/logger.h>
 #include <log4cplus/loggingmacros.h>
 #include <log4cplus/configurator.h>
 #include <log4cplus/consoleappender.h>
+#include <log4cplus/ndc.h>
+
+#include <httpserver.hpp>
 
 using namespace std;
 using namespace log4cplus;
+using namespace httpserver;
 
 void configureLogger()
 {
@@ -20,12 +25,31 @@ void configureLogger()
     logger.setLogLevel(INFO_LOG_LEVEL);
 }
 
+class StartingServer : public http_resource {
+private:
+    Logger logger = Logger::getInstance("StartingServer");
+
+public:
+    const shared_ptr<http_response> render(const http_request &request)
+    {
+        NDCContextCreator ndc(request.get_requestor() + ":" + to_string(request.get_requestor_port()));
+        LOG4CPLUS_INFO(logger, "Handling request");
+        return make_shared<string_response>("Test");
+    };
+};
+
 int main()
 {
     configureLogger();
 
     auto logger = Logger::getRoot();
     LOG4CPLUS_INFO(logger, "Backend started");
+
+    webserver ws = create_webserver(5000);
+    StartingServer server;
+    ws.register_resource("/", &server);
+    ws.start(true);
+
 
     return EXIT_SUCCESS;
 }
